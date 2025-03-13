9d2badd0a1205e16486b0a18c8eb403f71014d98
Thiago Marques Simões <lerreg22@gmail.com>
2025-03-12 22:56:44 -0300

    correções e ajustes, teste funcionando

--- consumer.py
+++ consumer.py
@@ -2,13 +2,13 @@ from Kafka import KafkaConsumer
 import json
 
 consumer = KafkaConsumer(
-    'clients',
+    'temperature_sensor_topic',
     api_version=(3,8,0),
     bootstrap_servers='kafka:9092',
     value_deserializer=lambda v: json.loads(v.decode('utf-8')),
     auto_offset_reset='earliest',
     enable_auto_commit=True,
-    group_id='clients_consumer_group'
+    group_id='temperature_sensor_consumer_group'
 )
 
 if __name__ == '__main__':
